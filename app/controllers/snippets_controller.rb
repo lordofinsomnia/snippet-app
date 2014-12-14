70d17644f144fb09de274859bf6ec7b6b7626e24
Lord Of Insomnia <mickom669@yahoo.com>
2014-12-14 13:57:09 +0100

    edits a snippet

--- app/controllers/snippets_controller.rb
+++ app/controllers/snippets_controller.rb
@@ -1,11 +1,26 @@
 class SnippetsController < ApplicationController
   def index
+  	@snippet  = Snippet.new
   	@snippets = Snippet.all
   end
 
   def create
     @snippet = Snippet.create(params[:snippet].permit(:filename, :content))
     redirect_to :back
+  end
+
+  def edit
+  	@snippet = Snippet.find params[:id]
+  end
+
+  def update 	
+  	snippet = Snippet.find params[:id]
+  	logger.debug 'test1'  	
+  	if snippet.update(params[:snippet].permit(:filename, :content))  	
+  		redirect_to snippets_path, :notice => 'Your snippet has successfully been updated.'
+  	else  		
+  		redirect_to :back, :notice => 'There was an error updating your snippet.'
+  	end
   end
 
   private
