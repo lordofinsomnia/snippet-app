94601f92d2600928c8d6ca916de6d711cba7d968
Lord Of Insomnia <mickom669@yahoo.com>
2014-12-16 11:17:08 +0100

    snipet tests failed rake db:migrate didnot work, used rake db:reset

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,14 @@ ActiveRecord::Schema.define(version: 20141216091916) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "snippets", force: true do |t|
+    t.string   "filename"
+    t.text     "content"
+    t.text     "output"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
