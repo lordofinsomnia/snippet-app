23a5eb46774da0936ea79bf3b34efb212baacf3d
Lord Of Insomnia <mickom669@yahoo.com>
2014-12-16 21:45:16 +0100

    comment model generated

--- app/models/snippet.rb
+++ app/models/snippet.rb
@@ -1,4 +1,6 @@
 class Snippet < ActiveRecord::Base
 	validates :filename, presence: true
 	validates :content, presence: true
+	has_many :comments, dependent: :destroy
+	belongs_to :user
 end
---
+++ spec/factories/comments.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :comment do
+    commenter "MyString"
+body "MyText"
+snippet nil
+  end
+
+end
