2f4fbe84918bb3d5ac1400114e85f3edc5ebb61d
Lord Of Insomnia <mickom669@yahoo.com>
2014-12-16 18:17:17 +0100

    refactoring completed

--- app/controllers/snippets_controller.rb
+++ app/controllers/snippets_controller.rb
@@ -5,18 +5,10 @@ class SnippetsController < ApplicationController
   end
 
   def create
-    @snippet = Snippet.create(params[:snippet].permit(:filename, :content))
-    uploaded_io = params[:file]        
-    
-    filename = Rails.root.join('files', 'uploads', uploaded_io.original_filename)
-    File.open(filename, 'wb') do |file|
-      file.write(uploaded_io.read)      
-      @snippet.filename = uploaded_io.original_filename      
-    end    
-    File.open(filename, 'rb') do |file2|      
-      @snippet.content = file2.read      
-      File.delete(file2)      
-    end  
+    @snippet           = Snippet.create(params[:snippet].permit(:filename, :content))  
+    @snippet.content   = find_file_content
+    @snippet.filename  = find_filename
+
     redirect_to :back, :notice => 'Your snippet has successfully been added.'
   end
 
@@ -25,26 +17,9 @@ class SnippetsController < ApplicationController
   end
 
   def update 	
-  	snippet       = Snippet.find params[:id] 
-    uploaded_io   = params[:file]
-    new_content   = String.new
-    new_name      = String.new
-    
-    if uploaded_io  
-      filename = Rails.root.join('files', 'uploads', uploaded_io.original_filename)
-      File.open(filename, 'wb') do |file|
-        file.write(uploaded_io.read)      
-        #snippet.filename = uploaded_io.original_filename
-        new_name = uploaded_io.original_filename
-      end    
-      File.open(filename, 'rb') do |file2|      
-        new_content = file2.read      
-        File.delete(file2)      
-      end  
-    else
-      new_name     = :filename
-      file_content = :content
-    end
+  	snippet       = Snippet.find params[:id]     
+    new_content   = find_file_content
+    new_name      = find_filename
 
   	if snippet.update(params[:snippet].permit(new_name, new_content))  	    
   		redirect_to snippets_path, :notice => 'Your snippet has successfully been updated.'
@@ -62,5 +37,39 @@ class SnippetsController < ApplicationController
 		def snippet_params
 		  params.require(:filename, :content).permit(:filename, :content)
 		end
+
+    def find_file_content      
+      new_content   = String.new      
+      uploaded_io   = params[:file]
+      if uploaded_io  
+        filename = Rails.root.join('files', 'uploads', uploaded_io.original_filename)        
+        File.open(filename, 'wb') do |file|
+          file.write(uploaded_io.read)
+        end
+        File.open(filename, 'rb') do |file2|          
+          new_content = file2.read      
+          File.delete(file2)      
+        end  
+      else        
+        new_content = :content
+      end
+      return new_content
+    end
+
+    def find_filename      
+      new_name      = String.new
+      uploaded_io   = params[:file]
+      if uploaded_io  
+        filename = Rails.root.join('files', 'uploads', uploaded_io.original_filename)
+        File.open(filename, 'wb') do |file|
+          file.write(uploaded_io.read)
+          new_name = uploaded_io.original_filename
+          File.delete(file)
+        end            
+      else
+        new_name     = :filename        
+      end
+      return new_name
+    end
 
 end
