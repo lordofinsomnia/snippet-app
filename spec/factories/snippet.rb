777aab97b5914bdfe72b98a53d4e3126ab5086cb
Lord Of Insomnia <mickom669@yahoo.com>
2014-12-17 15:31:44 +0100

    tmp comit

--- spec/factories/snippet.rb
+++ spec/factories/snippet.rb
@@ -1,7 +1,7 @@
-FactoryGirl.define do
+FactoryGirl.define do 
   factory :snippet do
     filename "testsnippet.rb"
     content "test snippet"
     output ""
-  end
+  end 
 end
--- spec/features/users/snippets_spec.rb
+++ spec/features/users/snippets_spec.rb
@@ -30,6 +30,36 @@ feature 'Snippets' do
 
     expect(page).to have_content 'Your snippet has successfully been added.'    
   end
+
+=begin
+  scenario "edits a snippet with user" do 
+    user = FactoryGirl.create(:user)
+    login_as(user, :scope => :user)
+    go_to_all_snippets       
+    visit snippets_path
+
+    @snippet = FactoryGirl.create :snippet    
+    #edit_snippet_path(@snippet)
+   
+    click_link 'Edit'
+
+    current_path.should == edit_snippet_path(@snippet)
+
+    find_field('Filename').value.should == 'testsnippet.rb'
+    find_field('Content').value.should == 'test snippet'
+
+    fill_in 'Filename', :with => 'testsnippet1.rb'
+    fill_in 'Content', :with => 'update snippet'
+
+    click_button 'Update Snippet'
+
+    current_path.should == snippet_path(@snippet)
+
+    page.should have_content 'testsnippet1.rb'
+    page.should have_content 'update snippet'     
+    page.should have_content 'Your snippet has successfully been updated.'
+  end
+=end
   
 end
 
